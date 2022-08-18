b6a450aa2306601f033e4f57485d3b1ec6ac03ec
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-18 18:11:07 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
+    #setter method
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    #getter
+    def name
+        @this_dogs_name
+    end
 
 end
+
+my_new_dog = Dog.new
+my_new_dog.name = "Bambuuee"
+puts my_new_dog.name
+
